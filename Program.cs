4cf6757d0d021128a4d7ed1d18234dfcfa16a7ea
dennisaguilar10 <dennis.aguilar10@gmail.com>
2026-08-29 09:52:03 -0300

    Remove configuração HTTPS para Railway

--- Program.cs
+++ Program.cs
@@ -3,42 +3,22 @@ using EcommerceApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Forçar porta 8080 para o Railway
-builder.WebHost.UseUrls("http://*:8080");
-
-// Configurar Kestrel SOMENTE para desenvolvimento
-if (builder.Environment.IsDevelopment())
-{
-    builder.WebHost.ConfigureKestrel(options =>
-    {
-        options.ListenLocalhost(5188); // HTTP
-        options.ListenLocalhost(7188, listenOptions =>
-        {
-            listenOptions.UseHttps();
-        });
-    });
-}
-
+// Configuração do banco de dados
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Serviços necessários para a API
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Swagger apenas em desenvolvimento
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
-
-// Só usar HTTPS redirection em desenvolvimento
-if (app.Environment.IsDevelopment())
-{
-    app.UseHttpsRedirection();
 }
 
 app.MapControllers();
