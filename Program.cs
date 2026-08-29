597211523f160a0e5b38ee947da0b29eb9290457
dennisaguilar10 <dennis.aguilar10@gmail.com>
2026-08-29 10:11:48 -0300

    Habilita Swagger em produção para testes no Railway

--- Program.cs
+++ Program.cs
@@ -15,11 +15,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Swagger apenas em desenvolvimento
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.MapControllers();
 
