9ecebd72c7b18d36dda8a2b0391e7a08c1d07850
dennisaguilar10 <dennis.aguilar10@gmail.com>
2026-09-09 17:57:31 -0300

    Adiciona autenticação JWT com Usuarios

--- obj/Debug/net9.0/EcommerceApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcommerceApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe27157469d7d78c3b40afd96530cc0d14ac6e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58e53e82178cb40576dd588a32ccf4005a9c800b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
